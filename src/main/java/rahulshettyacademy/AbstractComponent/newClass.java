245be13ef91d129a12d43496f03f2e4666cdfb75
Akshay6776 <6776akshay@gmail.com>
2024-02-04 22:55:25 +0530

    commit change

--- src/main/java/rahulshettyacademy/AbstractComponent/newClass.java
+++ src/main/java/rahulshettyacademy/AbstractComponent/newClass.java
@@ -4,6 +4,11 @@ public class newClass {
 
 	public static void main(String[] args) {
 		System.out.println("new class");
+		
+		System.out.println("new class");
+		
+		
+		System.out.println("new class");
 
 	}
 
